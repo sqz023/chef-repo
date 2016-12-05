9dbca41128f7852db1f60ad89f31956039aa410b
Saqib Zaheer <saqibzaheer144@gmail.com>
2016-12-04 21:02:04 -0600

    created first cookbook

---
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: first_cookbook
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'Install pachage httpd' do
+  package_name 'httpd'
+end
--- x
+++
@@ -1,2 +0,0 @@
-Hi
-This is the first change I'm going to push to my github account
